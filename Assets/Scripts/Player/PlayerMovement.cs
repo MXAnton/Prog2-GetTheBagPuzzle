3196788bf33d4c60ae6fb8f82a05d642a38de19c
Anton Lehrberg <anton.lehrberg@guteskolan.se>
2023-01-27 09:23:35 +0100

    Added running

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -30,6 +30,11 @@ public class PlayerMovement : MonoBehaviour
     private void Move() {
         Vector3 _moveDir = (movementInput.y * transform.forward) + (movementInput.x * transform.right);
         _moveDir *= movementSpeed * Time.fixedDeltaTime;
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            _moveDir *= runMultiplier;
+        }
+
         _moveDir.y = rb.velocity.y; // Don't change gravity or jump force
         rb.velocity = _moveDir;
     }
