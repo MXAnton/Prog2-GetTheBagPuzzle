7a4bc99afe8be5ccaefab916c94bc3e9aeef0dec
MXAnton <anton.lehrberg@hotmail.se>
2023-02-09 10:42:44 +0100

    Added player health.

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private float health;
+    [SerializeField]
+    private float maxHealth = 100;
+
+    private void Start() {
+        SetPlayerHealth(maxHealth);
+    }
+
+    public void SetPlayerHealth(float _newHealth) {
+        health = _newHealth;
+    }
+
+    public void AddPlayerHealth(float _amount) {
+        float _newHealth = health + _amount;
+        if (_newHealth > maxHealth) {
+            _newHealth = maxHealth;
+        }
+        health = _newHealth;
+    }
+    public void RemovePlayerHealth(float _amount) {
+        float _newHealth = health - _amount;
+        if (_newHealth <= 0) {
+            _newHealth = 0;
+        }
+        health = _newHealth;
+
+        if (health <= 0) {
+            // Player dead
+            Die();
+        }
+    }
+
+    private void Die() {
+        Debug.Log("Player dead");
+    }
+}
